47f2658db13a12a058788fa030897c39e5c45a7d
Taylor Gibb <taylor@developerhut.co.za>
2022-04-15 13:15:22 +0200

    updating build scripts

--- build-generation/generate.js
+++ build-generation/generate.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs-extra");
+const git = require("simple-git");
 const handlebars = require("handlebars");
-const home = process.env["GITHUB_WORKSPACE"];
+const home = process.env["GITHUB_WORKSPACE"] || "../";
 const action_root = path.join(home, "build-generation");
 
 var config = JSON.parse(
@@ -10,18 +11,73 @@ var config = JSON.parse(
 
 for (let i = 0; i < config.length; i++) {
   const project = config[i];
-  console.log(`Project: ${project.name}`);
+  console.log(`Generating Build: ${project.name}`);
+
+  let base_output_path = path.join(home, "output", `${project.name}`);
+
   for (var configuration in project.client.configurations) {
     var build_configuration = project.client.configurations[configuration];
-    console.log(build_configuration);
 
     var options = {
-        name: project.name,
-        branch: build_configuration.branch
-    }
-    var template = fs.readFileSync(path.join(action_root, "template", "template.yml"));
-    const render = handlebars.compile(template, { noEscape: true })
-    var output = render(options)
-    console.log(output)
+      name: project.name,
+      branch: build_configuration.branch,
+      build_workflow: "./dh-build",
+      build_environment: configuration,
+    };
+
+    console.log(options);
+
+    var template = fs
+      .readFileSync(path.join(action_root, "template", "template.yml"))
+      .toString();
+
+    const render = handlebars.compile(template, { noEscape: true });
+    var output = render(options);
+
+    let workflow_output_path = path.join(
+      base_output_path,
+      ".github",
+      "workflows",
+      `${project.name}-${configuration}.yml`
+    );
+    let build_output_path = path.join(
+      base_output_path,
+      ".github",
+      "workflows",
+      "dh-build",
+      "action.yml"
+    );
+
+    //write the workflow to disk
+    fs.mkdirSync(path.dirname(workflow_output_path), { recursive: true });
+    fs.writeFileSync(workflow_output_path, output);
+
+    //write dh-build to disk
+    fs.mkdirSync(path.dirname(build_output_path), { recursive: true });
+    fs.copyFileSync(
+      path.join(action_root, "workflows", "build", `${project.build}.yml`),
+      build_output_path
+    );
   }
+
+  //clone project and commit files
+  var repo_path = path.join(home, "repositories", project.name);
+  if (fs.existsSync(repo_path)) {
+    fs.rmSync(repo_path, { recursive: true });
+  }
+  fs.mkdirSync(repo_path, { recursive: true });
+
+  console.log(`Cloning Repository To: ${repo_path}`);
+
+  git()
+    .clone(project.repository, repo_path)
+    .then(() => {
+      console.log("Copying Build Files");
+      fs.copySync(base_output_path, repo_path, { recursive: true });
+      git()
+        .add('./*')
+        .commit("updating build scripts")
+        .addRemote("origin", "master")
+        .push(["-u", "origin", "master"], () => console.log("done"));
+    });
 }
