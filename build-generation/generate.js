036aea648c4ef392f8e6a1e2f2946664f0f7f107
Taylor Gibb <taylor@developerhut.co.za>
2022-04-15 10:03:47 +0200

    a

--- build-generation/generate.js
+++ build-generation/generate.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
-const path = require('path');
-const core = require('@actions/core');
-const github = require('@actions/github');
-const home = process.env['GITHUB_WORKSPACE']
+const path = require("path");
+const core = require("@actions/core");
+const github = require("@actions/github");
+const home = process.env["GITHUB_WORKSPACE"];
 
+var config = JSON.parse(
+  fs.readFileSync(
+    path.join(home, "build-generation", "configuration", "config.json"),
+    "utf8"
+  )
+);
 
-var config = JSON.parse(fs.readFileSync(path.join(home, "configuration", "config.json"), "utf8"));
 console.log(config);
