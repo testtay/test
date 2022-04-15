2c9d4aa38c580792acceea257789bf363c50f753
Taylor Gibb <taylor@developerhut.co.za>
2022-04-15 09:12:18 +0200

    t

---
+++ .github/workflows/generate.js
@@ -0,0 +1,7 @@
+
+try {
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
